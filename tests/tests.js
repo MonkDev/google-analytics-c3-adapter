1019e9956adc6407582718979797a7886cd68e56
Kenny Kaye <kenny@kaye.us>
2014-12-18 16:41:30 -0800

    Adds additional assertions to test suite

--- tests/tests.js
+++ tests/tests.js
@@ -13,29 +13,60 @@ describe('gac3', function () {
     multiSourceNoKeys = { sources: [timeData1, timeData2] };
   });
 
+  describe('gac3 global', function () {
+    it('should be bound to the window object', function () {
+      assert.isObject(window.gac3, 'gac3 is bound to the window object');
+    });
+  });
+
   describe('gac3.columns()', function () {
 
     describe('with single-source data with no keys', function () {
-      it('should return a single array', function () {
-        assert.isArray(gac3.columns(singleSourceNoKeys), 'columns are a collection');
+      it('should return a collection of arrays', function () {
+        var results = gac3.columns(singleSourceNoKeys);
+        assert.isArray(results, 'columns are a collection');
+        assert.lengthOf(results, 3, 'columns has length of 3');
+        results.forEach(function (result) {
+          assert.isString(result[0], 'column key is a string');
+          assert.isNumber(result[1], 'column value');
+        });
       });
     });
 
     describe('with single-source data with keys', function () {
       it('should return collection of arrays', function () {
-        assert.isArray(gac3.columns(singleSourceUserKeys), 'columns are a collection');        
+        var results = gac3.columns(singleSourceUserKeys);
+        assert.isArray(results, 'columns are a collection');
+        assert.lengthOf(results, 1, 'columns has length of 1');
+        results[0].forEach(function (value, index) {
+          if(index === 0) {
+            assert.isString(value, 'first element is a key');
+          } else {
+            assert.isNumber(value, 'data point is a number');
+          }
+        });
       });
     });
 
     describe('with multi-source data with keys', function () {
       it('should return collection of arrays', function () {
-        assert.isDefined(gac3.columns(multiSourceUserKeys), 'columns have been formatted');
-        assert.isArray(gac3.columns(multiSourceUserKeys), 'columns are a collection');
+        var results = gac3.columns(multiSourceUserKeys);
+        assert.isArray(results, 'columns are a collection');
+        assert.lengthOf(results, 2, 'columns has length of 2');
+        results.forEach(function (result) {
+          result.forEach(function (value, index) {
+            if(index === 0) {
+              assert.isString(value, 'first element is a key');
+            } else {
+              assert.isNumber(value, 'data point is a number');
+            }
+          });
+        });
       });
     });
 
     describe('with multi-source data with no keys', function () {
-      it('should throw a custom error', function () {
+      it('should throw a RangeError', function () {
         var fn = function () { gac3.columns(multiSourceNoKeys); };
         expect(fn).to.throw(RangeError);
       });
